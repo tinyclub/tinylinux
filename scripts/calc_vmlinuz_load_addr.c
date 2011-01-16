03bbd2ba29088db0ba9d29d4dbb6aa223d51e00c
Wu Zhangjin <wuzhangjin@gmail.com>
2011-01-16 16:09:34 +0800

    MIPS: Clean the compressed kernel support
    Changes:
    	o Rename ld.script to vmlinuz.lds to make it be compiled
    	automatically.
    	o Move calc_vmlinuz_load_addr to scripts to make it be compiled
    	previously
    	o Remove duplication of vmlinuz and vmlinuz.unsstrip
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- scripts/calc_vmlinuz_load_addr.c
+++ scripts/calc_vmlinuz_load_addr.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 "Wu Zhangjin" <wuzhangjin@gmail.com>
+ * Copyright (C) 2010, 2011 "Wu Zhangjin" <wuzhangjin@gmail.com>
  *
  * This program is free software; you can redistribute  it and/or modify it
  * under  the terms of  the GNU General  Public License as published by the
