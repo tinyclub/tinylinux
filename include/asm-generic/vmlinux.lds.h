2d8e6681403d4b9c9f92cab16e0114ef4558519b
Wu Zhangjin <wuzhangjin@gmail.com>
2011-08-02 00:34:26 +0800

    gc-sections: Merge .rodata.* to .rodata
    The regular expression [A-Za-z0-9_$^]* doesn't work well currently, use
    .rodata.* instead, but we need to fix the regular expression at last...
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -253,6 +253,7 @@
 	.rodata           : AT(ADDR(.rodata) - LOAD_OFFSET) {		\
 		VMLINUX_SYMBOL(__start_rodata) = .;			\
 		*(.rodata)						\
+		*(.rodata.*)						\
 		*(.rodata.[A-Za-z0-9_$^]*)				\
 		*(__vermagic)		/* Kernel version magic */	\
 		TRACE_MARKERS_STRINGS()	/* Markers: strings */		\
